6f30df50606ba82b16c9db3991be7902665b5755
a.lazev@transdata.no <a.lazev@transdata.no>
2023-03-23 00:41:54 +0200

    Main init app

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    from .auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+    from .main import bp as main_bp
+    app.register_blueprint(main_bp)
     return app
 
 
