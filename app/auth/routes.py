b5a1768656b590a8a2f9248574848a2a4f312352
a.lazev@transdata.no <a.lazev@transdata.no>
2023-03-23 03:38:18 +0200

    routes for auth

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,11 +1,25 @@
 from app.auth import bp
+from flask import render_template, redirect
+from .forms import LoginForm, RegisterForm
 
 
 @bp.route('/')
 def index():
-    return 'It is auth!'
+    return render_template('auth/index.html', title='Auth')
 
-#
-# @bp.login('/login')
-# def login():
-#     pass
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/index')
+    return render_template('auth/login.html', title='Login', form=form)
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return redirect('/index')
+    return render_template('auth/register.html', title='Register', form=form)
+
