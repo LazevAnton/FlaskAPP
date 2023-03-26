9b7434dc4a6bed83d0daf3d036a1d031452dada8
a.lazev@transdata.no <a.lazev@transdata.no>
2023-03-26 11:42:34 +0300

    PEP8

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -10,14 +10,16 @@ from wtforms import (
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[validators.DataRequired()])
-    password = PasswordField('Password', validators=[validators.DataRequired()])
+    username = StringField('Username', validators=[validators.DataRequired(message='Username is required')])
+    password = PasswordField('Password', validators=[validators.DataRequired(message='Password is required'),
+                                                     validators.Length(min=8,
+                                                                       message='Min 6 length of password is required')])
     remember = BooleanField('Remember')
     submit = SubmitField('Login')
 
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[validators.DataRequired()])
-    email = EmailField('Email', validators=[validators.DataRequired()])
+    email = EmailField('Email', validators=[validators.DataRequired(), validators.Email()])
     password = PasswordField('Password', validators=[validators.DataRequired()])
     submit = SubmitField('Register')
