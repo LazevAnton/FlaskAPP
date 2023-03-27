25155e5a468bef0d1cf465cceb017401d5a2f384
a.lazev@transdata.no <a.lazev@transdata.no>
2023-03-27 15:54:59 +0300

    SQLAlchemy,Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,31 @@
 from flask import Flask
 from flask_login import current_user
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    db.init_app(app)
+    migrate.init_app(app, db)
     from .auth import bp as auth_bp
     app.register_blueprint(auth_bp)
     from .main import bp as main_bp
     app.register_blueprint(main_bp)
+    from .models import User
 
     @app.context_processor
     def context_processor():
         return dict(
             current_user=current_user
         )
+
     return app
 
 
