5f26ed502fad2e67f0da2885833e24c9e38ad5da
a.lazev@transdata.no <a.lazev@transdata.no>
2023-03-27 15:54:28 +0300

    INSER USERS INTO DB

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,10 +1,37 @@
 from app.main import bp
 from flask import render_template
+from app.models import User
+from app import db
 
 
 @bp.route('/')
 @bp.route('/index')
 def index():
+    flask_users = [
+        {
+            'username': 'test10',
+            'email': 'test10@gmail.com',
+            'password': 'passwdtest1'
+        },
+        {
+            'username': 'test11',
+            'email': 'test11@gmail.com',
+            'password': 'passwdtest2'
+        },
+        {
+            'username': 'test12',
+            'email': 'test12@gmail.com',
+            'password': 'passwdtest3'
+        },
+    ]
+    for data in flask_users:
+        user_data = User(
+            username=data.get('username'),
+            email=data.get('email'),
+            password=data.get('password')
+        )
+        db.session.add(user_data)
+    db.session.commit()
     return render_template('index.html', title='FlaskAPP')
 
 
