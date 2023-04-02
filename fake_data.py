63654776f91ce5fbe3a1e67e7d4e56f3664cec0e
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-02 17:46:08 +0300

    generate fakes users data for testing

---
+++ fake_data.py
@@ -0,0 +1,32 @@
+import click
+from app import db
+from flask import Blueprint
+from faker import Faker
+from app.models import User
+
+bp = Blueprint('fake', __name__)
+faker = Faker()
+
+
+@bp.cli.command("users")
+@click.argument('num', type=int)
+def create_fake_users(num):
+    users = []
+    username = faker.user_name()
+    email = faker.email()
+    for user in range(num):
+        user = (
+            db.session.query(User).filter(
+                User.username == username,
+                User.email == email
+            )
+        ).first()
+        if not user:
+            user = User(
+                username=username,
+                email=email
+            )
+        db.session.add('user')
+        users.append(user)
+    db.session.commit()
+    print(num, 'users added ')
