55176df35226b16d029688a2259922b3a28e0795
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-02 12:18:08 +0300

    edit password

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, index=True)
     email = db.Column(db.String, unique=True, index=True)
-    password = db.Column(db.String, index=True)
+    password = db.Column(db.String, nullable=False)
 
     def __repr__(self):
         return f'{self.username} - {self.email}'
