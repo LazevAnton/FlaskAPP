020e551aa053d95095427cd61767693fed0186fb
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-02 17:46:23 +0300

    Add password_hash

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -6,6 +7,12 @@ class User(db.Model):
     username = db.Column(db.String, unique=True, index=True)
     email = db.Column(db.String, unique=True, index=True)
     password = db.Column(db.String, nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
     def __repr__(self):
         return f'{self.username} - {self.email}'
