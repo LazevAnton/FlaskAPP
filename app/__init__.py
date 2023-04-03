f4e961bc66ea3615102dbfd0d19358180d0bbcf2
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-03 23:46:00 +0300

    Add login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
-from flask_login import current_user
+from flask_login import current_user,LoginManager
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
 
 
 def create_app():
@@ -20,6 +22,11 @@ def create_app():
     from .models import User  # noqa
     from .fake_data import bp as fake_bp
     app.register_blueprint(fake_bp)
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.get(user_id)
 
     @app.context_processor
     def context_processor():
