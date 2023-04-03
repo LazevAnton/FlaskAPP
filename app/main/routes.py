c7122a078d25609bf6a53d847122e8df69e3fddb
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-03 23:45:42 +0300

    Edite def index

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,38 +7,42 @@ from app import db
 @bp.route('/')
 @bp.route('/index')
 def index():
-    flask_users = [
-        {
-            'username': 'test10',
-            'email': 'test10@gmail.com',
-            'password': 'passwdtest1'
-        },
-        {
-            'username': 'test11',
-            'email': 'test11@gmail.com',
-            'password': 'passwdtest2'
-        },
-        {
-            'username': 'test12',
-            'email': 'test12@gmail.com',
-            'password': 'passwdtest3'
-        },
-    ]
-    for data in flask_users:
-        user_data = db.session.query(User).filter(
-            User.username == data.get('username'),
-            User.email == data.get('email')
-        )
-        if user_data:
-            continue
-        user_data = User(
-            username=data.get('username'),
-            email=data.get('email'),
-            password=data.get('password')
-        )
-        db.session.add(user_data)
-    db.session.commit()
-    return render_template('index.html', title='FlaskAPP')
+    user_query = db.session.query(User)
+    users = user_query
+    print(users)
+    return render_template('index.html', user=users)
+    # flask_users = [
+    #     {
+    #         'username': 'test10',
+    #         'email': 'test10@gmail.com',
+    #         'password': 'passwdtest1'
+    #     },
+    #     {
+    #         'username': 'test11',
+    #         'email': 'test11@gmail.com',
+    #         'password': 'passwdtest2'
+    #     },
+    #     {
+    #         'username': 'test12',
+    #         'email': 'test12@gmail.com',
+    #         'password': 'passwdtest3'
+    #     },
+    # ]
+    # for data in flask_users:
+    #     user_data = db.session.query(User).filter(
+    #         User.username == data.get('username'),
+    #         User.email == data.get('email')
+    #     )
+    #     if user_data:
+    #         continue
+    #     user_data = User(
+    #         username=data.get('username'),
+    #         email=data.get('email'),
+    #         password=data.get('password')
+    #     )
+    #     db.session.add(user_data)
+    # db.session.commit()
+    # return render_template('index.html', title='FlaskAPP')
 
 
 @bp.route('/about')
