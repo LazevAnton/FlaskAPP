ec485e7f2d3ffca288a56c96fd98dc08ccef4865
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-05 09:46:20 +0300

    profile + edit_profile

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,3 +1,4 @@
+from app.auth.forms import EditProfileForm
 from app.main import bp
 from flask import render_template
 from app.models import User
@@ -8,9 +9,10 @@ from app import db
 @bp.route('/index')
 def index():
     user_query = db.session.query(User)
-    users = user_query
+    users = user_query.all()
     print(users)
     return render_template('index.html', user=users)
+
     # flask_users = [
     #     {
     #         'username': 'test10',
@@ -48,3 +50,14 @@ def index():
 @bp.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@bp.route('/profile')
+def profile():
+    return render_template('profile.html', title='Profile')
+
+
+@bp.route('/edit_profile', methods=['GET', 'POST'])
+def edit_profile():
+    form = EditProfileForm()
+    return render_template('edit_profile.html', form=form)
