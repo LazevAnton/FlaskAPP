d738f9bb9849f5b5b8bd97f1cbfc34372aca6bce
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-06 00:23:51 +0300

    Edit def index

--- app/main/routes.py
+++ app/main/routes.py
@@ -11,40 +11,7 @@ def index():
     user_query = db.session.query(User)
     users = user_query.all()
     print(users)
-    return render_template('index.html', user=users)
-
-    # flask_users = [
-    #     {
-    #         'username': 'test10',
-    #         'email': 'test10@gmail.com',
-    #         'password': 'passwdtest1'
-    #     },
-    #     {
-    #         'username': 'test11',
-    #         'email': 'test11@gmail.com',
-    #         'password': 'passwdtest2'
-    #     },
-    #     {
-    #         'username': 'test12',
-    #         'email': 'test12@gmail.com',
-    #         'password': 'passwdtest3'
-    #     },
-    # ]
-    # for data in flask_users:
-    #     user_data = db.session.query(User).filter(
-    #         User.username == data.get('username'),
-    #         User.email == data.get('email')
-    #     )
-    #     if user_data:
-    #         continue
-    #     user_data = User(
-    #         username=data.get('username'),
-    #         email=data.get('email'),
-    #         password=data.get('password')
-    #     )
-    #     db.session.add(user_data)
-    # db.session.commit()
-    # return render_template('index.html', title='FlaskAPP')
+    return render_template('index.html', users=users,title='Profile')
 
 
 @bp.route('/about')
