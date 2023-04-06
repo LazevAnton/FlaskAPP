d1eb7157174787295428d9d44cff7b1bc2e70d62
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-07 00:12:10 +0300

    PEP8

--- app/user/routes.py
+++ app/user/routes.py
@@ -1,30 +1,34 @@
 from flask_login import login_required
-from .forms import EditProfileForm
+from .forms import ProfileForm
 from app.user import bp
-from flask import render_template, flash
+from flask import render_template, flash, redirect, url_for, request
 from .. import db
 from ..models import User
 
 
-@bp.route('/profile')
+@bp.route('/index')
+def index():
+    return render_template('user/flaskapp.html', title='FlaskAPP')
+
+
+@bp.route('/profile/<string:username>', methods=['GET', 'POST'])
 @login_required
-def profile():
-    user_query = db.session.query(User)
-    users = user_query.all()
-    print(users)
-    return render_template('profile.html', users=users, title='Profile')
-
-
-@bp.route('/edit_profile/<string:username>', methods=['GET', 'POST'])
-def edit_profile(username):
+def profile(username):
     user = db.session.query(User).filter(User.username == username).first_or_404()
-    form = EditProfileForm()
+    form = ProfileForm()
     if form.validate_on_submit():
         user.profile.first_name = form.first_name.data
         user.profile.last_name = form.last_name.data
-        user.profile.linkedIn_url == form.linkedin_url.data
-        user.profile.facebook_url == form.facebook_url.data
-        user.profile.bio == form.bio.data
+        user.profile.linkedIn_url = form.linkedin_url.data
+        user.profile.facebook_url = form.facebook_url.data
+        user.profile.bio = form.bio.data
         db.session.commit()
-        flash('Your changes has been set', category='success')
-    return render_template('edit_profile.html', form=form)
+        flash('Your profile has been saved', category='success')
+        return redirect(url_for('user.profile', username=user.username))
+    elif request.method == 'GET':
+        form.first_name.data = user.profile.first_name
+        form.last_name.data = user.profile.last_name
+        form.linkedin_url.data = user.profile.linkedIn_url
+        form.facebook_url.data = user.profile.facebook_url
+        form.bio.data = user.profile.bio
+    return render_template('user/profile.html', title='Profile', form=form, user=user)
