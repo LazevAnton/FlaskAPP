72008e29077fc871305893c0faa0b029d86b1d37
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-08 13:34:43 +0300

    Create post def

--- app/post/routes.py
+++ app/post/routes.py
@@ -11,8 +11,8 @@ from app.post.forms import PostForm
 def create_post():
     form = PostForm()
     if request.method == 'POST':
-        if form.validate_on_submit():
-            post = Post(title=form.post_title.data, content=form.post_content.data, author=current_user)
+        if form.validate_on_submit:
+            post = Post(title=form.post_title, content=form.post_content, author=current_user)
             db.session.add(post)
             db.session.commit()
             flash('Excellent, your post has been created', category='success')
@@ -21,4 +21,4 @@ def create_post():
             if not title or len(title) < 2:
                 flash('Sorry, title must be at least two characters long', category='error')
         return redirect(url_for('user.blog'))
-    return render_template('user/blog.html', title='Create Post', form=form)
+    return render_template('user/blog.html', title='Create post', form=form)
