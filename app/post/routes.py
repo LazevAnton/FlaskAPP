735d05c1dff0c0f25f0ecafa3f6f6ac531c98224
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-08 18:59:09 +0300

    Create def like, dislike, delete

--- app/post/routes.py
+++ app/post/routes.py
@@ -1,7 +1,9 @@
+from os import abort
+
 from flask import request, flash, redirect, url_for, render_template
 from flask_login import login_required, current_user
 from app import db
-from app.models import Post
+from app.models import Post, Like, Dislike
 from app.post import bp
 from app.post.forms import PostForm
 
@@ -20,5 +22,42 @@ def create_post():
             title = form.post_title.data
             if not title or len(title) < 2:
                 flash('Sorry, title must be at least two characters long', category='error')
-        return redirect(url_for('user.blog'))
+        return redirect(url_for('user.blog', user=current_user))
     return render_template('user/blog.html', form=form, title='Create post')
+
+
+@bp.route('/<int:post_id>/like', methods=['GET', 'POST'])
+@login_required
+def like(post_id):
+    post = Post.query.get_or_404(post_id)
+    if Like.query.filter_by(user=current_user, post=post).count() > 0:
+        flash('Sorry, but you have already liked this post', category='warning')
+    post_like = Like(user=current_user, post=post)
+    db.session.add(post_like)
+    db.session.commit()
+    flash('Thank you, you have liked this post')
+    return redirect(request.referrer)
+
+
+@bp.route('/<int:post_id>/dislike', methods=['GET', 'POST'])
+@login_required
+def dislike(post_id):
+    post = Post.query.get_or_404(post_id)
+    if Like.query.filter_by(user=current_user, post=post).count() > 0:
+        flash('Sorry, but you have already disliked this post', category='warning')
+    post_dislike = Dislike(user=current_user, post=post)
+    db.session.add(post_dislike)
+    db.session.commit()
+    flash('Thank you, you have disliked this post')
+    return redirect(request.referrer)
+
+
+@bp.route('/<int:post_id>/delete', methods=['GET', 'POST'])
+def delete(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('You have deleted you post', category='success')
+    return redirect(url_for('user.blog'))
