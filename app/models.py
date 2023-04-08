54d7ef9f4578a08ce9feffd94a339a8245189241
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-08 11:49:52 +0300

    def avatar

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from hashlib import md5
 
 from app import db
 from flask_login import UserMixin
@@ -14,7 +15,20 @@ class User(BaseModel, UserMixin):
     username = db.Column(db.String, unique=True, index=True)
     email = db.Column(db.String, unique=True, index=True)
     password = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
     post = db.relationship('Post', backref='author', uselist=True, lazy="dynamic", cascade="all,delete")
+    likes = db.relationship('Like', backref='user', lazy='dynamic', primaryjoin='User.id=Like.user_id',
+                            cascade='all, delete')
+    dislikes = db.relationship('Dislike', backref='user', lazy='dynamic', primaryjoin='User.id=Dislike.user_id',
+                               cascade='all, delete')
+
+    followers = db.relationship('Follow', backref='followee', foreign_keys='Follow.followee_id')
+    following = db.relationship('Follow', backref='follow', foreign_keys='Follow.follow_id')
+
+    def avatar(self, size):
+        avatar_hash = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{avatar_hash}?d=identicon&s={size}'
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -55,3 +69,51 @@ class Post(BaseModel):
         db.ForeignKey('user.id', name='fk_posts_author_id', ondelete='CASCADE'),
         nullable=False
     )
+    likes = db.relationship('Like', backref='post', uselist=True, cascade='all, delete')
+    dislikes = db.relationship('Dislike', backref='post', uselist=True, cascade='all, delete')
+
+
+class Like(BaseModel):
+    __tablename__ = 'likes'
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', name='fk_likes_user_id'),
+        nullable=False
+    )
+    post_id = db.Column(
+        db.Integer,
+        db.ForeignKey('post.id', name='fk_likes_post_id'),
+        nullable=False
+    )
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Dislike(BaseModel):
+    __tablename__ = 'dislikes'
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', name='fk_dislikes_user_id'),
+        nullable=False
+    )
+    post_id = db.Column(
+        db.Integer,
+        db.ForeignKey('post.id', name='fk_dislikes_post_id'),
+        nullable=False
+    )
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Follow(BaseModel):
+    __table_name__ = 'follows'
+    follow_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', name='fk_follows_follow_id'),
+        primary_key=True
+    )
+    followee_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', name='fk_follows_followee_id')
+    )
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
