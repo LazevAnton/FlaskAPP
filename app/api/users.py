8774329b4783c74c50b353b715c509e98aceb174
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-10 11:27:53 +0300

    API users class

---
+++ app/api/users.py
@@ -0,0 +1,11 @@
+from flask_restful import Resource
+from flask import jsonify
+from app.models import User
+from app.schemas import UserSchema
+from app import db
+
+
+class UsersResource(Resource):
+    def get(self):
+        users = db.session.query(User).all()
+        return jsonify(UserSchema(exclude=('password','id',)).dump(users, many=True))
