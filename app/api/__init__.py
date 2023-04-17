8992934a8d72f874f44a3d5bdf64cfdfc0d6a6bf
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-17 19:21:09 +0300

    Likes and DisLikes

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,8 @@
 from flask import Blueprint
 from flask_restful import Api
+
+from .dislike import DisLikesResource
+from .like import LikesResource
 from .users import UsersResource, UserResource
 from .posts import PostUserResource
 
@@ -9,3 +12,5 @@ api = Api(bp)
 api.add_resource(UsersResource, '/users', endpoint='users_list')
 api.add_resource(UserResource, '/users/<int:user_id>', endpoint='user_details')
 api.add_resource(PostUserResource, '/users/<int:user_id>/posts', endpoint='user_posts')
+api.add_resource(LikesResource, '/likes', endpoint='likes_list')
+api.add_resource(DisLikesResource, '/dislikes', endpoint='dislikes_list')
