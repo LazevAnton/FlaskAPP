ec6ee61b45bc7272812dc41771e5b9b45a6ee574
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-17 18:54:35 +0300

    PEP8

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api
-from .users import UsersResource
+from .users import UsersResource, UserResource
 from .posts import PostUserResource
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 api = Api(bp)
 
 api.add_resource(UsersResource, '/users', endpoint='users_list')
+api.add_resource(UserResource, '/users/<int:user_id>', endpoint='user_details')
 api.add_resource(PostUserResource, '/users/<int:user_id>/posts', endpoint='user_posts')
