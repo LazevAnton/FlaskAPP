9d63a83b318d3866c5cc0004b75b169618647c1f
a.lazev@transdata.no <a.lazev@transdata.no>
2023-05-02 01:49:36 +0300

    PEP8

--- app/post/__init__.py
+++ app/post/__init__.py
@@ -15,25 +15,26 @@ bp = Blueprint('post', __name__, url_prefix='/post')
 @click.argument('user_id', type=int)
 def extract_posts(user_id):
     post_info = []
-    user_name = db.session.query(User).filter(User.id==user_id).first()
+    user_name = db.session.query(User).filter(User.id == user_id).first()
     if user_name:
-        post_username = user_name.username
-    posts = db.session.query(Post).filter(Post.author_id == user_id).all()
-    for post in posts:
-        title = post.title
-        likes = len(post.likes)
-        dislikes = len(post.dislikes)
-        created = post.created_at
-        post_info.append(
-            {
-                'PostTitle': title,
-                'Likes': likes,
-                'Dislikes': dislikes,
-                'CreatedAT': created
-            }
-        )
-    df = pd.DataFrame(post_info, columns=['PostTitle', 'Likes', 'Dislikes', 'CreatedAT'])
-    df.to_csv(Path(config.Config.BASEDIR) / f'{post_username}_post_info.csv')
+        posts = db.session.query(Post).filter(Post.author_id == user_id).all()
+        for post in posts:
+            title = post.title
+            likes = len(post.likes)
+            dislikes = len(post.dislikes)
+            created = post.created_at
+            post_info.append(
+                {
+                    'PostTitle': title,
+                    'Likes': likes,
+                    'Dislikes': dislikes,
+                    'CreatedAT': created
+                }
+            )
+        df = pd.DataFrame(post_info, columns=['PostTitle', 'Likes', 'Dislikes', 'CreatedAT'])
+        df.to_csv(Path(config.Config.BASEDIR) / f'{user_name}_post_info.csv')
+    else:
+        print('Error! User didnt found')
 
 
 from . import routes  # noqa
