718af6e3b162c88ad02a38a1816edb212798d30b
a.lazev@transdata.no <a.lazev@transdata.no>
2023-05-02 01:45:55 +0300

    Create extract_posts

--- app/post/__init__.py
+++ app/post/__init__.py
@@ -1,4 +1,39 @@
+from pathlib import Path
+
+import click
 from flask import Blueprint
+import pandas as pd
+
+import config
+from .. import db
+from ..models import Post, User
 
 bp = Blueprint('post', __name__, url_prefix='/post')
+
+
+@bp.cli.command('extract_posts')
+@click.argument('user_id', type=int)
+def extract_posts(user_id):
+    post_info = []
+    user_name = db.session.query(User).filter(User.id==user_id).first()
+    if user_name:
+        post_username = user_name.username
+    posts = db.session.query(Post).filter(Post.author_id == user_id).all()
+    for post in posts:
+        title = post.title
+        likes = len(post.likes)
+        dislikes = len(post.dislikes)
+        created = post.created_at
+        post_info.append(
+            {
+                'PostTitle': title,
+                'Likes': likes,
+                'Dislikes': dislikes,
+                'CreatedAT': created
+            }
+        )
+    df = pd.DataFrame(post_info, columns=['PostTitle', 'Likes', 'Dislikes', 'CreatedAT'])
+    df.to_csv(Path(config.Config.BASEDIR) / f'{post_username}_post_info.csv')
+
+
 from . import routes  # noqa
