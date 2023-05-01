787e1507c117f53e0cb74d6bb9af1bf9ce67b9f4
a.lazev@transdata.no <a.lazev@transdata.no>
2023-05-02 00:56:24 +0300

    Add extract_users

--- app/user/__init__.py
+++ app/user/__init__.py
@@ -1,5 +1,29 @@
+from pathlib import Path
+
 from flask import Blueprint
+from sqlalchemy import func
+
+import config
+from .. import db
+from ..models import User, Profile
+import pandas as pd
 
 bp = Blueprint('user', __name__, url_prefix='/user')
 
+
+@bp.cli.command('extract_users')
+def extract_users():
+    user_data = []
+    users_info = db.session.query(User).all()
+    for user in users_info:
+        user_data.append(
+            {
+                'UserName': user.username,
+                'Email': user.email,
+                'FullName': user.profile.fullname,
+                'PostCount': user.posts.count()
+            }
+        )
+    df = pd.DataFrame(user_data, columns=['UserName', 'Email', 'FullName', 'PostCount'])
+    df.to_csv(Path(config.Config.basedir)/ 'users.csv')
 from . import routes  # noqa
